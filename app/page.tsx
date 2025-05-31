a27de31f8da9a62c432ecba1a653b4f4bd5eac46
Akmal Khan <akmalnawabi007@gmail.com>
2025-05-31 20:29:48 +0330

    new

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 'use client'
 
 import { Button } from "@/components/ui/button";
+import * as actions from '@/app/actions'
 
 export default function Home() {
   return (
     <div>
-      <Button className="bg-slate-700">click me</Button>
+      <form action={actions.signOut}>
+        <Button type="submit" className="bg-slate-700">Sign In</Button>
+      </form>
+
+      <form action={actions.signOut}>
+        <Button type="submit" className="bg-slate-700">Sign Out</Button>
+      </form>
     </div>
   );
 }
